214d1aed4b649d6568815f67147786f20126551b
Pedro Rodrigues <4513618+pemsbr@users.noreply.github.com>
2021-03-30 23:21:58 +0100

    Add zshenv with exists() and link with dotbot

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,7 +1,11 @@
 #!/usr/bin/env zsh
 
-echo "\n<<< Starting Homebrew Setup >>>\n"
+if exists brew; then
+  echo "brew exists, skipping install"
+else
+  echo "brew doesn't exist, continuing with install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew bundle --verbose
