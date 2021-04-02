4885c4d60b1d8662761b0d6dbf0ab9421d383b60
Pedro Rodrigues <4513618+pemsbr@users.noreply.github.com>
2021-04-02 02:30:35 +0100

    Add setup_xcode.zsh

--- setup_homebrew.zsh
+++ setup_homebrew.zsh
@@ -1,29 +1,5 @@
 #!/usr/bin/env zsh
 
-XCODE_VERSION=$(xcodebuild -version | awk 'NR==1{print $2}')
-ACCEPTED_LICENSE_VERSION=$(defaults read /Library/Preferences/com.apple.dt.Xcode IDEXcodeVersionForAgreedToGMLicense)
-readonly XCODE_VERSION
-readonly ACCEPTED_LICENSE_VERSION
-
-# Install Xcode Command Line Tools
-if ! exists xcode-select --print-path; then
-  xcode-select --install &> /dev/null
-
-  # Wait until the Xcode Command Line Tools are installed
-  until exists xcode-select --print-path; do
-    sleep 5
-  done
-
-  echo "Xcode command line tools installed"
-fi
-
-# Accept Xcode license
-if ! [[ $XCODE_VERSION = "$ACCEPTED_LICENSE_VERSION" ]]; then  
-  echo "Xcode license needs to be accepted."
-  sudo xcodebuild -license accept
-  sudo xcodebuild -runFirstLaunch
-fi
-
 # Install Homebrew
 if exists brew; then
   echo "Skipping Homebrew install. It is already installed."
